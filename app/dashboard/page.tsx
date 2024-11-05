34991d1da6e61d4d276203950b428a28aad742cb
sgfsf88 <sgfsf88@gmail.com>
2024-11-05 17:45:25 +0900

    카드데이터 추가

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,30 @@
-export default function dashboardPage() {
-  return <div>텍스트 수정함</div>;
+import { Card } from '@/app/ui/dashboard/cards';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
+
+export default async function dashboardPage() {
+  const {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
+  return (
+    <main>
+      <h2 className={`${lusitana.className} text-[24px] mb-[16px]`}>
+        DashBoard
+      </h2>
+      <div className="grid grid-cols-4 gap-6 md:grid-cols-2 sm:grid-cols-1">
+        <Card title="collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+    </main>
+  );
 }
